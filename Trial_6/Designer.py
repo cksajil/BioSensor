af3d0eaa988ab7b16810e65888b2965b768b90ea
cksajil <sajilck@gmail.com>
2018-02-05 13:35:38 +0530

    Add Trial 6 Output

--- Trial_6/Designer.py
+++ Trial_6/Designer.py
@@ -2,8 +2,8 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 
-width  		= 	9.3
-height	    = 4.14
+width  		= 	12
+height	    = 5
 #height 		= 	width / 1.618
 labelsize   =   20
 
@@ -14,7 +14,8 @@ plt.rc('ytick', labelsize = labelsize)
 plt.rc('axes', labelsize  = labelsize)
 
 
-GlucoseLevels 	= 	list(np.arange(3.5, 12.5, 1.0))
+# GlucoseLevels 	= 	list(np.arange(3.5, 13.5, 1.0))
+GlucoseLevels 	= 	list(np.arange(3.5, 7.5, 1.0))
 Low  			= 	3.5
 High  			= 	6.5
 Step   			=   0.5
@@ -23,7 +24,8 @@ IndexL       	= 	GlucoseLevels.index(Low)
 IndexH       	= 	GlucoseLevels.index(High)
 Gselected  = np.arange(Low, High+0.5, 2*Step)
 
-data = pd.read_csv('output_20px.csv', index_col=0)
+# data = pd.read_csv('output_SetA.csv', index_col=0)
+data = pd.read_csv('output_SetB.csv', index_col=0)
 data = data.iloc[:,range(IndexL, IndexH+1, int(Step*2))]
 
 
@@ -37,12 +39,12 @@ axes, lines  = data.boxplot(sym='+', return_type='both')
 
 plt.xlabel('Glucose Concentration($mmolL^{-1}$)')
 plt.ylabel('Intensity($a.u.$)')
-#plt.ylim(75, 255)
+# plt.ylim(220, 260)
 plt.grid(False)
 #plt.show()
 fig1.set_size_inches(width, height)
-fig1.savefig('Results/Trial5_Box_'+str(Low)+'_'+str(High)+'.svg', dpi=300)
-
+# fig1.savefig('Results/Trial6_SetA_Box_'+str(Low)+'_'+str(High)+'.svg', dpi=300)
+fig1.savefig('Results/Trial6_SetB_Box_'+str(Low)+'_'+str(High)+'.png', dpi=300)
 
 fig2, ax2 = plt.subplots()
 fig2.subplots_adjust(left=.12, bottom=.15, right=.95, top=.97)
@@ -58,9 +60,10 @@ plt.xticks(Gselected)
 
 plt.xlabel('Glucose Concentration($mmolL^{-1}$)')
 plt.ylabel('Mean value of red component')
-#plt.ylim(100, 255)
+# plt.ylim(220, 260)
 #plt.grid(True)
 #plt.show()
 
 fig2.set_size_inches(width, height)
-fig2.savefig('Results/Trial5_MeanRed_'+str(Low)+'_'+str(High)+'.svg', dpi=300)
+# fig2.savefig('Results/Trial6_SetA_MeanRed_'+str(Low)+'_'+str(High)+'.svg', dpi=300)
+fig2.savefig('Results/Trial6_SetB_MeanRed_'+str(Low)+'_'+str(High)+'.png', dpi=300)
--- Trial_6/Image_analysis.py
+++ Trial_6/Image_analysis.py
@@ -2,18 +2,23 @@
 
 from os import listdir
 
-imagesrc = 'Samples/20px'
+#imagesrc = 'Samples/SetA'
+imagesrc = 'Samples/SetB'
 files = listdir(imagesrc)
 files = sorted(files)
 
+
 print files
+
 
 # Input Glucose Concentration Information
 
 import numpy as np
-gluccose_levels = np.arange(3.5, 12.5, 1.0)
+#gluccose_levels = np.linspace(3.5, 12.5, 10)
+gluccose_levels = np.linspace(3.5, 6.5, 4)
 
 print gluccose_levels
+
 
 # Read Images
 
@@ -39,9 +44,12 @@ for file in files:
 # Transpose the matrix 
 data = map(list, zip(*data)) 
 
+print type(data)
+
 import pandas as pd
 
 #columns=gluccose_levels
 
 result = pd.DataFrame(data, columns = gluccose_levels )
-result.to_csv('output_20px.csv', sep=',')
+#result.to_csv('output_SetA.csv', sep=',')
+result.to_csv('output_SetB.csv', sep=',')
